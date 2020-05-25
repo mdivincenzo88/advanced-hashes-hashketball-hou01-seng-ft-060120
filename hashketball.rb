e5c7281cb605a67495e0892caa7cabd010869276
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-25 16:52:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,6 +180,18 @@ end
 return final_array
 end
 
+def player_stats(player_name)
+final_array =[]
+# Write code here
+game_hash.each do  |team_location, info|
+  if info[:team_name] == team_name
+       info[:players].each do |index|
+         final_array.push (index[:number])
+end
+end
+end
+return final_array
+end
 
 
 
